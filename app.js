01a46c59e95cce43078f8dd1be68bb0398ec47db
JMcode97 <joseandresmontano@gmail.com>
2023-01-17 22:13:30 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+bar = document.getElementById('responsive-icon');
+bar.onclick = function() {
+    links = document.querySelector('.menu-navbar');
+    links.classList.toggle('active');
+
+    // socialLinks = document.getElementById('menu-socials');
+    // socialLinks.classList.toggle('active');
+}
