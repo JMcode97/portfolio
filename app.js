f9da7b68850541ff0ce421dc0ec12b09f7d7eecd
Jose Montaño <103230766+JMcode97@users.noreply.github.com>
2023-06-28 14:20:35 -0400

    Updates (#2)
    * Delete CNAME
    
    * Minify HTML/CSS (#1)
    
    * Delete CNAME
    
    * Minify HTML/CSS & add project
    
    * Portfolio fav-ico
    
    Co-authored-by: JMcode97 <joseandresmontano@gmail.com>
    
    * Fix project img
    
    * update
    
    ---------
    
    Co-authored-by: JMcode97 <joseandresmontano@gmail.com>

--- app.js
+++ app.js
@@ -2,7 +2,4 @@ bar = document.getElementById('responsive-icon');
 bar.onclick = function() {
     links = document.querySelector('.menu-navbar');
     links.classList.toggle('active');
-
-    // socialLinks = document.getElementById('menu-socials');
-    // socialLinks.classList.toggle('active');
 }
